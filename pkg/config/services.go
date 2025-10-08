379fbb74d1049bfdee2d2c6dfbeb7f143d606d58
Ben Kochie <superq@gmail.com>
2025-10-08 17:32:29 +0200

    Merge pull request #1693 from bdeore/add-regex-cassandra
    Add DimensionRegexps for AWS/Cassandra

--- pkg/config/services.go
+++ pkg/config/services.go
@@ -206,6 +206,10 @@ var SupportedServices = serviceConfigs{
 		ResourceFilters: []*string{
 			aws.String("cassandra"),
 		},
+		DimensionRegexps: []*regexp.Regexp{
+			regexp.MustCompile("keyspace/(?P<Keyspace>[^/]+)/table/(?P<TableName>[^/]+)"),
+			regexp.MustCompile("keyspace/(?P<Keyspace>[^/]+)/"),
+		},
 	},
 	{
 		Namespace: "AWS/CloudFront",
